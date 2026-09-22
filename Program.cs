717cc52037cb49a8c22eda9c9f00f65c1e544fa9
antonplusdlc <serverbsvvv@gmail.com>
2026-09-22 13:57:17 +0300

    Lab05: basic while loops - countdown and sum

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("obratniy otscet");
+
+int countdown = 5;
+while (countdown >= 1)
+{
+    Console.WriteLine(countdown);
+    countdown--;
+}
+Console.WriteLine("start!");
+
+Console.WriteLine();
+Console.WriteLine("summa ot 1 do 10");
+
+int num = 1;
+int sum = 0;
+
+while (num <= 10)
+{
+    sum += num;
+    num++;
+}
+
+Console.WriteLine($"sum: {sum}");
