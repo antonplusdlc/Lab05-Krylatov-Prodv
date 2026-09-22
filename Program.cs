e1760b0a97d0a5373210d3b9315b4366dd160098
antonplusdlc <serverbsvvv@gmail.com>
2026-09-22 14:02:43 +0300

    Lab05:infinite loopdemo and while input validation

--- Program.cs
+++ Program.cs
@@ -21,3 +21,31 @@ while (num <= 10)
 }
 
 Console.WriteLine($"sum: {sum}");
+
+Console.WriteLine();
+Console.WriteLine("beskonechii while");
+
+int i = 1;
+while (i <= 5)
+{
+    Console.WriteLine($"{i}");
+    i++;
+}
+
+Console.WriteLine();
+Console.WriteLine("Validation vvoda cherez while");
+bool isValid = false;
+int enteredAge = 0;
+
+while (!isValid)
+{
+    Console.Write("enter your age (int, not float!): ");
+    string input = Console.ReadLine();
+    isValid = int.TryParse(input, out enteredAge);
+
+    if (!isValid)
+    {
+        Console.WriteLine("ITS NOT INT");
+    }
+}
+Console.WriteLine($"vozrast: {enteredAge}");
